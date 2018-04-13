c0fe2e1ee17e75a9005855e119c8df6f5886fc3f
Ajri Sylhasi <ajrisylhasi@hotmail.com>
2018-04-13 17:36:48 +0200

    Full Version 2.0 - Removed all buttons and added automatic recognition for the arduino.

--- app/src/main/java/org/bonevet/ajri_communication/MainActivity.java
+++ app/src/main/java/org/bonevet/ajri_communication/MainActivity.java
@@ -1,31 +1,25 @@
 package org.bonevet.ajri_communication;
 
 import android.annotation.SuppressLint;
+import android.content.IntentFilter;
+import android.hardware.usb.UsbManager;
 import android.os.Bundle;
-import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
-import android.text.Html;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
-import android.widget.TextView;
-import android.widget.Toast;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
 
 import com.physicaloid.lib.Physicaloid;
 import com.physicaloid.lib.usb.driver.uart.ReadLisener;
 
-import java.io.UnsupportedEncodingException;
-
-
-
 public class MainActivity extends AppCompatActivity {
 
-
     private static final String TAG = MainActivity.class.getSimpleName();
-    Button btOpen, btClose, btRelays, btRead;
-    TextView vl_intensity, vl_voltage, vl_battery, vl_temperature, vl_range, vl_speed, vl_rpm;
-    String ard_read;
-    String[] vlerat;
+    Button btRelays;
     Physicaloid mPhysicaloid;
 
     @SuppressLint("SetTextI18n")
@@ -33,121 +27,33 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        btRelays= (Button) findViewById(R.id.btRelays);
-        btOpen  = (Button) findViewById(R.id.btOpen);
-        btClose = (Button) findViewById(R.id.btClose);
-        btRead = (Button) findViewById(R.id.btRead);
-        vl_intensity = (TextView) findViewById(R.id.vl_intensity);
-        vl_voltage = (TextView) findViewById(R.id.vl_voltage);
-        vl_battery = (TextView) findViewById(R.id.vl_battery);
-        vl_temperature = (TextView) findViewById(R.id.vl_temperature);
-        vl_range = (TextView) findViewById(R.id.vl_range);
-        vl_speed = (TextView) findViewById(R.id.vl_speed);
-        vl_rpm = (TextView) findViewById(R.id. vl_rpm);
-        setEnabledUi(false);
+        btRelays = (Button) findViewById(R.id.btRelays);
         mPhysicaloid = new Physicaloid(this);
         mPhysicaloid.setBaudrate(9600);
         btRelays.setTag(1);
-        btRelays.setText("Lidhu");
-        btRelays.setBackgroundResource(R.drawable.round1);
-
-
+        IntentFilter filter = new IntentFilter();
+        filter.addAction(UsbManager.ACTION_USB_DEVICE_ATTACHED);
+        filter.addAction(UsbManager.ACTION_USB_DEVICE_DETACHED);
+        registerReceiver(mUsbReceiver, filter);
+        setEnabledUi(false);
     }
-
-    public void onClickOpen(View v) {
-        if(mPhysicaloid.open()) {
-            setEnabledUi(true);
-            btRelays.setText("Ndalur");
-        } else {
-            btRelays.setText("Lidhu");
-//            Toast.makeText(this, "Nuk Mund Te Lidhet", Toast.LENGTH_SHORT).show();
-        }
-    }
-
-    public void onClickRead(View v){
-        byte[] buf = new byte[256];
-        ard_read =new String("");
-        vlerat   = new String[0];
-        int readSize=0;
-        readSize = mPhysicaloid.read(buf);
-        if(readSize>0) {
-
-            try {
-                ard_read = new String(buf, "UTF-8");
-            }
-            catch (Exception e) {
-                e.printStackTrace();
-            }
-            vlerat = ard_read.split("-");
-            vl_intensity.setText(vlerat[0] + " A");
-            vl_voltage.setText(vlerat[1] + " V");
-            vl_battery.setText(vlerat[2] + " %");
-            vl_temperature.setText(vlerat[3] + " ℃");
-            vl_range.setText(vlerat[4] + " km");
-            vl_speed.setText(vlerat[5] + " km/h");
-            vl_rpm.setText(vlerat[6] + " rpm's");
-        }
-        mPhysicaloid.clearReadListener();
-        btRead.setEnabled(false);
-        new Handler().postDelayed(new Runnable() {
-            @Override
-            public void run() {
-                btRead.setEnabled(true);
-            }
-        }, 1000);
-    }
-//    private void read() {
-//        byte[] buf = new byte[256];
-//        ard_read =new String("");
-//        vlerat   = new String[0];
-//        int readSize=0;
-//        readSize = mPhysicaloid.read(buf);
-//        if(readSize>0) {
-//
-//            try {
-//                ard_read = new String(buf, "UTF-8");
-//            }
-//            catch (Exception e) {
-//                e.printStackTrace();
-//            }
-//            mPhysicaloid.clearReadListener();
-//
-//            vlerat = ard_read.split("-");
-//            vl_intensity.setText(vlerat[0] + " A");
-//            vl_voltage.setText(vlerat[1] + " V");
-//            vl_battery.setText(vlerat[2] + " %");
-//            vl_temperature.setText(vlerat[3] + " ℃");
-//            vl_range.setText(vlerat[4] + " km");
-//            vl_speed.setText(vlerat[5] + " km/h");
-//            vl_rpm.setText(vlerat[6] + " rpm's");
-//        }
-//        btRead.setEnabled(false);
-//        new Handler().postDelayed(new Runnable() {
-//            @Override
-//            public void run() {
-//                btRead.setEnabled(true);
-//            }
-//        }, 1500);
-//    }
 
     public void onClickSwitch(View v) {
-        final int status =(Integer) v.getTag();
-        if(status == 1) {
+        final int status = (Integer) v.getTag();
+        if (status == 1) {
             btRelays.setText("Kontakt");
             btRelays.setBackgroundResource(R.drawable.round2);
             String str = "1";
             byte[] buf = str.getBytes();
             mPhysicaloid.write(buf, buf.length);
-            btRead.setEnabled(true);
             v.setTag(2);
-        } else if (status ==2) {
+        } else if (status == 2) {
             btRelays.setText("Ndezur");
             btRelays.setBackgroundResource(R.drawable.round3);
             String str = "2";
             byte[] buf = str.getBytes();
             mPhysicaloid.write(buf, buf.length);
             v.setTag(3);
-            btRead.setEnabled(true);
         } else {
             btRelays.setText("Ndalur");
             btRelays.setBackgroundResource(R.drawable.round1);
@@ -155,52 +61,65 @@ public class MainActivity extends AppCompatActivity {
             byte[] buf = str.getBytes();
             mPhysicaloid.write(buf, buf.length);
             v.setTag(1);
-            btRead.setEnabled(false);
-            vl_intensity.setText("NoN");
-            vl_voltage.setText("NoN");
-            vl_battery.setText("NoN");
-            vl_temperature.setText("NoN");
-            vl_range.setText("NoN");
-            vl_speed.setText("NoN");
-            vl_rpm.setText("NoN");
-        }
-    }
-    public void onClickClose(View v) {
-        if(mPhysicaloid.close()) {
-            mPhysicaloid.clearReadListener();
-            setEnabledUi(false);
-            btRelays.setText("Lidhu");
         }
     }
 
     private void setEnabledUi(boolean on) {
-        if(on) {
-            btOpen.setEnabled(false);
-
-            btClose.setEnabled(true);
-
+        if (on) {
             btRelays.setEnabled(true);
         } else {
-            btOpen.setEnabled(true);
+            btRelays.setClickable(false);
+            btRelays.setBackgroundResource(R.drawable.round1);
+            btRelays.setText("Lidhu");
 
-            btClose.setEnabled(false);
-
-            btRelays.setEnabled(false);
-
-            btRead.setEnabled(false);
         }
     }
 
-    Handler mHandler = new Handler();
-    private void tvAppend(TextView tv, String text) {
-        final TextView ftv = tv;
-        ftv.setText("");
-        final String ftext = text;
-        mHandler.post(new Runnable() {
-            @Override
-            public void run() {
-                ftv.append(ftext);
-            }
-        });
+    private void closeDevice() {
+        if(mPhysicaloid.close()) {
+            setEnabledUi(false);
+            mPhysicaloid.clearReadListener();
+        }
     }
+
+    private void openDevice() {
+        if (!mPhysicaloid.isOpened()) {
+            if (mPhysicaloid.open()) { // default 9600bps
+                setEnabledUi(true);
+                btRelays.setClickable(true);
+            }
+
+        }
+        else {
+            setEnabledUi(false);
+            btRelays.setClickable(false);
+            btRelays.setBackgroundResource(R.drawable.round1);
+            btRelays.setText("Lidhu");
+        }
+    }
+
+    protected void onNewIntent(Intent intent) {
+        String action = intent.getAction();
+
+        if (UsbManager.ACTION_USB_DEVICE_ATTACHED.equals(action)) {
+            openDevice();
+            setEnabledUi(true);
+            btRelays.setText("Ndalur");
+
+        }
+    }
+
+    BroadcastReceiver mUsbReceiver = new BroadcastReceiver() {
+        public void onReceive(Context context, Intent intent) {
+            String action = intent.getAction();
+
+            if (UsbManager.ACTION_USB_DEVICE_DETACHED.equals(action)) {
+                closeDevice();
+                mPhysicaloid.clearReadListener();
+                setEnabledUi(false);
+                btRelays.setText("Lidhu");
+                btRelays.setBackgroundResource(R.drawable.round1);
+            }
+        }
+    };
 }
